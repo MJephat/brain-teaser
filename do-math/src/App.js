6160ab9c5884c46eebf6d542fda5d691516dbafe
MJephat <mukundijephat22@gmail.com>
2023-04-27 15:32:22 +0300

    added some styling, funtionalities

--- do-math/src/App.js
+++ do-math/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+// import '../style.css';
+
 import Addition from './component/add';
 
 function App() {
   return (
     <div className="App">
-     <h1> DO MATH </h1>
+      
      <Addition />
     </div>
   );
--- do-math/src/component/add.js
+++ do-math/src/component/add.js
@@ -15,15 +15,16 @@ function Addition(){
     }
 
     return(
-        <div>
-            <form className='form'>
+        <div className='return'>
+            {/* <form className='form'> */}
+            <h1> DO MATH </h1>
                 <input type="number" placeholder="0"id='first' value= {number1} onChange={e => setNumber1(+e.target.value)}/>
                 <input type="number" placeholder='0' id='first' value = {number2} onChange={e => setNumber2(+e.target.value)}/>
 
-            </form>
+            {/* </form> */}
             <button id="button" onClick={handleMath}> Add Them</button>
 
-            <h2>{output}</h2>
+            <h2 id="text">Total : {output}</h2>
 
         </div>
 
